5cbcc8048ae7deb78107d3d4886060bccc8b9bd3
Umxn2 <121830671+Umxn2@users.noreply.github.com>
2024-10-02 12:44:36 +0530

    Update colors.lua

--- lua/doom-one/colors.lua
+++ lua/doom-one/colors.lua
@@ -7,7 +7,7 @@
 
 local colors = {
   dark = {
-    bg = "#282c34",
+    bg = "#0f111a",
     fg = "#bbc2cf",
 
     bg_alt = "#21242b",
@@ -24,16 +24,16 @@ local colors = {
 		base8 = "#dfdfdf",
 
 		grey = "#3f444a",
-		red = "#ff6c6b",
+		red = "#ec675d",
 		orange = "#da8548",
-		green = "#98be65",
+		green = "#88f398",
 		teal = "#4db5bd",
 		yellow = "#ecbe7b",
-		blue = "#51afef",
+		blue = "#75c4f9",
 		dark_blue = "#2257a0",
 		magenta = "#c678dd",
 		violet = "#a9a1e1",
-		cyan = "#46d9ff",
+		cyan = "#65a9d6",
 		dark_cyan = "#5699af",
   },
 	light = {
